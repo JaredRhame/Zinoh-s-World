674f4e7efc9321966cf0991bbe6e2b07c0d8b514
JaredRhame <[jared.rhame@gmail.com]>
2018-06-24 06:17:55 -0400

    Created Projects page.(Not Done) Created sticky Nav.(Not fully linked)

--- script.js
+++ script.js
@@ -1,4 +1,8 @@
-var loopingTxt = ['create', 'zone out to music', 'play videos games', 'learn'];
+let loopingTxt = ['create', 'zone out to music', 'play videos games', 'learn'];
+// Get the navbar
+const navbar = document.getElementById("navbar");
+// Get the offset position of the navbar
+const sticky = navbar.offsetTop;
 
 textSequence(0);
 function textSequence(i) {
@@ -13,4 +17,17 @@ function textSequence(i) {
         textSequence(0);
     }
 
+}
+
+// When the user scrolls the page, execute myFunction
+window.onscroll = function() {myFunction()};
+
+// Add the sticky class to the navbar when you reach its scroll position. Remove "sticky" when you leave the scroll position
+function myFunction() {
+  if (window.pageYOffset >= sticky) {
+    navbar.classList.add("sticky");
+    // navbar.classList.remove("not-sticky");
+  } else {
+    navbar.classList.remove("sticky");
+  }
 }
