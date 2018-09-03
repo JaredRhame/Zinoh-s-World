d58b796042e13d42bd86251482092e40cea05dbe
JaredRhame <[jared.rhame@gmail.com]>
2018-09-03 18:19:10 -0400

    Fixed the modal for all projects. Reworked the js for the modal actions. Added the first project image.

--- src/projects.js
+++ src/projects.js
@@ -1,5 +1,10 @@
 // Get the modal
 const modal1 = document.getElementById('modal-1');
+const modal2 = document.getElementById('modal-2');
+const modal3 = document.getElementById('modal-3');
+const modal4 = document.getElementById('modal-4');
+const modal5 = document.getElementById('modal-5');
+const modal6 = document.getElementById('modal-6');
 
 // Get the button that opens the modal
 const btn1 = document.getElementById("btn-1");
@@ -9,23 +14,41 @@ const btn4 = document.getElementById("btn-4");
 const btn5 = document.getElementById("btn-5");
 const btn6 = document.getElementById("btn-6");
 
+const buttons = [btn1, btn2, btn3, btn4, btn5, btn6];
 
+let closing = (e) =>{
+  e.target.parentElement.style.display = "none";
+}
+
+// let modalDisplay = (e) =>{
+//   e.target
+// }
 // Get the <span> element that closes the modal
-const span = document.getElementsByClassName("close")[0];
+let span = document.querySelectorAll(".close");
 
 // When the user clicks the button, open the modal
 btn1.addEventListener('click', () =>{
   modal1.style.display = "flex";
 });
-
-// When the user clicks on <span> (x), close the modal
-span.addEventListener('click', () =>{
-  modal1.style.display = "none";
+btn2.addEventListener('click', () =>{
+  modal2.style.display = "flex";
 });
-
-// When the user clicks anywhere outside of the modal, close it
-window.addEventListener('click', () =>{
-  if (event.target == modal1 || event.target == document.body) {
-      modal1.style.display = "none";
-  }
-})
+btn3.addEventListener('click', () =>{
+  modal3.style.display = "flex";
+});
+btn4.addEventListener('click', () =>{
+  modal4.style.display = "flex";
+});
+btn5.addEventListener('click', () =>{
+  modal5.style.display = "flex";
+});
+btn6.addEventListener('click', () =>{
+  modal6.style.display = "flex";
+});
+// When the user clicks on <span> (x), close the modal
+span[0].addEventListener('click', closing);
+span[1].addEventListener('click', closing);
+span[2].addEventListener('click', closing);
+span[3].addEventListener('click', closing);
+span[4].addEventListener('click', closing);
+span[5].addEventListener('click', closing);
